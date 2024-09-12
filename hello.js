e8188fd5db9e50cc722d33855dafdf96dd93dcd5
pawanvs <30742783+pawanvs@users.noreply.github.com>
2024-09-12 07:10:18 -0700

    added certificates

--- hello.js
+++ hello.js
@@ -1,14 +1,25 @@
-const http = require('http');
+// const http = require('http');
+const https = require('https');
 
 const host = '0.0.0.0';
 const port = 5000;
+var fs = require('fs');
 
-const server = http.createServer((req, res) => {
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('niv/privkey1.pem'),
+  cert: fs.readFileSync('niv/fullchain1.pem'),
+  ca:fs.readFileSync('niv/chain1.pem')
+};
+
+const server = https.createServer(options, (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('si frontend port 5000');
+  res.end('si backend port 5000');
 });
 
 server.listen(port, host, () => {
-   console.log('Web server running at http://%s:%s',host,port );
+   console.log('SI Back running at http://%s:%s',host,port );
 });
+
+
